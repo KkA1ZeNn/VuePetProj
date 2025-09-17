94a7342328152b3d1b118eff7ac87e0ed0bad160
manuilov <manuilov@bolid.ru>
2025-09-17 15:21:41 +0300

    Добавил компонент заголовка

--- vueviteproject/src/App.vue
+++ vueviteproject/src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import CalcPage from './components/CalcPage.vue';
 </script>
 
 <template>
-  <div>
-    <h1>Test Project</h1>
+  <div class="app_container">
+    <CalcPage />
   </div>
 </template>
 
 <style scoped>
+  .app_container {
+    width: 100vw;
+    height: 100vh;
+    background-color: #fff;
+    padding: 30px;
+    box-sizing: border-box;
+  }
 </style>
---
+++ vueviteproject/src/components/CalcPage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="calc-page_container">
+        <header class="calc-page_header">
+            <h1 class="calc-page_title">Калькулятор затрат</h1>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+    .calc-page_container {
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 32px;
+    }
+
+    .calc-page_header {
+        width: 100%;
+        height: 100px;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .calc-page_title {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 31.1px;
+        line-height: 36px;
+        color: #000000;
+    }
+</style>
